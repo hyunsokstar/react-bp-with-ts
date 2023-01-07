6eb3c4646f3801b2954ea8ecf59280fda777a617
hyunsokstar <hyunsokstar@gmail.com>
2023-01-07 22:17:45 +0900

    update for ts

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import { ThemeProvider } from "styled-components";
 import App from "./App";
 import { darkTheme } from "./theme";
-
+import { createRoot } from "react-dom/client";
 
 // const darkTheme = {
 //     textColor: "whitesmoke",
@@ -15,12 +15,21 @@ import { darkTheme } from "./theme";
 //     backgroundColor: "whitesmoke",
 // };
 
-ReactDOM.render(
-    <React.StrictMode>
-        <ThemeProvider theme={darkTheme}>
-            <App />
-        </ThemeProvider>
-    </React.StrictMode>,
-    document.getElementById("root")
-);
+// ReactDOM.render(
+//     <React.StrictMode>
+//         <ThemeProvider theme={darkTheme}>
+//             <App />
+//         </ThemeProvider>
+//     </React.StrictMode>,
+//     document.getElementById("root")
+// );
 
+const container = document.getElementById("root");
+const root = createRoot(container!); // createRoot(container!) if you use TypeScript
+root.render(
+  <React.StrictMode>
+    <ThemeProvider theme={darkTheme}>
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
+);
--- src/theme.ts
+++ src/theme.ts
@@ -3,8 +3,11 @@ import { DefaultTheme } from "styled-components";
 export const lightTheme: DefaultTheme = {
   bgColor: "white",
   textColor: "black",
+  accentColor: "#9c88ff",
+
 };
 export const darkTheme: DefaultTheme = {
   bgColor: "black",
   textColor: "white",
+  accentColor: "#9c88ff",
 };
